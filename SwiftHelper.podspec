4fb5ac8f436fdab6cb9b11e0eb589e0ae4b280c6
liuchang <itlc@qq.com>
2023-12-22 14:17:27 +0800

    4.1.0

--- SwiftHelper.podspec
+++ SwiftHelper.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "SwiftHelper"
-  s.version      = "4.0.0"
+  s.version      = "4.1.0"
   s.summary      = "Some extensions in Swift."
   s.homepage     = "https://github.com/iLiuChang/SwiftHelper"
   s.license      = "MIT"
--- SwiftHelper/UIKit/UIColor+SH.swift
+++ SwiftHelper/UIKit/UIColor+SH.swift
@@ -10,7 +10,12 @@ import UIKit
 
 public extension UIColor {
     
-    convenience init(hexString: String) {
+    public enum AlphaPosition {
+        case leading
+        case trailing
+    }
+    
+    convenience init(hexString: String, alphaPosition: AlphaPosition = .leading) {
         var red:   CGFloat = 0.0
         var green: CGFloat = 0.0
         var blue:  CGFloat = 0.0
@@ -30,19 +35,39 @@ public extension UIColor {
                 green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
                 blue  = CGFloat(hexValue & 0x00F)              / 15.0
             case 4:
-                red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
-                green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
-                blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
-                alpha = CGFloat(hexValue & 0x000F)             / 15.0
+                switch alphaPosition {
+                case .leading:
+                    red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
+                    green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
+                    blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
+                    alpha = CGFloat(hexValue & 0x000F)             / 15.0
+                    break
+                case .trailing:
+                    alpha = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
+                    red   = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
+                    green = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
+                    blue  = CGFloat(hexValue & 0x000F)             / 15.0
+                    break
+                }
             case 6:
                 red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
                 green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
                 blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
             case 8:
-                red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
-                green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
-                blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
-                alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
+                switch alphaPosition {
+                case .leading:
+                    red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+                    green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+                    blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
+                    alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
+                    break
+                case .trailing:
+                    alpha = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+                    red   = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+                    green = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
+                    blue  = CGFloat(hexValue & 0x000000FF)         / 255.0
+                    break
+                }
             default:
                 print("Invalid RGB string")
             }
